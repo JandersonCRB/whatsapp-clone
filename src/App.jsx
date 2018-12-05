e933e33670f1f7aada66f895168f5aba62d67fa0
JandersonCRB <jandersonangelo@hotmail.com>
2018-12-05 09:07:37 -0300

    add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import firebase from 'firebase';
 
 import Main from './Main/Main';
 
 class App extends Component {
+  state = {
+    user: null,
+  }
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDgq63qfEMuU6eR7-U-r1OtEOuGt_H16MU",
+      authDomain: "whatsapp-clone-f8371.firebaseapp.com",
+      databaseURL: "https://whatsapp-clone-f8371.firebaseio.com",
+      projectId: "whatsapp-clone-f8371",
+      storageBucket: "whatsapp-clone-f8371.appspot.com",
+      messagingSenderId: "244554863163"
+    };
+    firebase.initializeApp(config);
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(_ => {
+        firebase.auth().signInWithPopup(provider).then(result => {
+          const { uid, photoURL, displayName, email, emailVerified } = result.user;
+          const user = {
+            uid,
+            photoURL,
+            displayName,
+            email,
+            emailVerified,
+          }
+          this.setState({ user });
+          const db = firebase.firestore();
+          db.collection("users").doc(uid).set(user)
+            .then(_ => {
+              console.log('Signed in');
+            })
+            .catch(error => {
+              console.error("Error writing document: ", error);
+            })
+        }).catch(function(error) {
+          console.error(error);
+        });
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        console.error(error)
+      });
+    const provider = new firebase.auth.GoogleAuthProvider();
+  }
+  
   render() {
+    const { user } = this.state;
     return (
       <>
         <CssBaseline />
-        <Main />
+        <Main user={user}/>
       </>
     );
   }
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,47 +1,53 @@
 import React, { Component } from 'react';
 
+import firebase from 'firebase';
+
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import MessageIcon from '@material-ui/icons/Message';
 import SearchIcon from '@material-ui/icons/Search';
 
-const chats = [
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-	{ img: require('./avatar.jpg'), name: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
-]
+// let chats = [
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// 	{ photoURL: require('./avatar.jpg'), displayName: 'Janderson Angelo', lastMsg: 'coe rapaziada'},
+// ]
 
 class Main extends Component {
 	state = {
+		chats: [],
+		msg: '',
 		currentChat: null,
 	}
 
@@ -49,8 +55,19 @@ class Main extends Component {
 		this.setState({ currentChat: index });
 	}
 
+	componentDidMount() {
+		const db = firebase.firestore();
+		db.collection("users").get()
+			.then(snapshot => {
+				const chats = snapshot.docs.map(user => user.data());
+				this.setState({ chats });
+			})
+	}
+	
+
 	render() {
-		const { currentChat } = this.state;
+		const { currentChat, chats, msg } = this.state;
+		console.log(chats)
 		return (
 			<Grid container style={{ maxHeight: '100vh' }}>
 				<Grid container item xs={4} style={{ height: '100%' }}>
@@ -82,12 +99,12 @@ class Main extends Component {
 						{chats.map((chat, index) => (
 							<ButtonBase key={index} onClick={_ => this.active(index)} disableRipple component={Grid} container direction="row" wrap="nowrap" alignItems="center" style={{ height: 72, maxWidth: '100%', overflow: 'hidden' }}>
 								<div style={{ width: 68, paddingLeft: 13, paddingRight: 15 }}>
-									<Avatar src={chat.img} />
+									<Avatar src={chat.photoURL} />
 								</div>
 								<Grid container direction="column" style={{ height: '100%', maxWidth: 'calc(100% - 68px)', borderTop: '1px solid #f2f2f2', paddingRight: 15, paddingTop: 10 }}>
 									<Grid container direction="row">
 										<Typography noWrap style={{ fontSize: 16, fontWeight: 500 }}>
-											{chat.name}
+											{chat.displayName}
 										</Typography>
 									</Grid>
 									<Grid container direction="row" style={{ marginTop: 3 }}>
@@ -101,8 +118,17 @@ class Main extends Component {
 					</Grid>
 				</Grid>
 				<Grid item xs={8} style={{ height: '100vh', backgroundColor: 'purple'}}>
-					ola
-					{currentChat}
+					{currentChat !== null && chats[currentChat].displayName}
+					<TextField value={msg} onChange={e => this.setState({ msg: e.target.value })} />
+					<Button variant="contained" onClick={_ =>{
+						const db = firebase.firestore();
+						console.log(this.props.user)
+						db.collection('messages').add({
+							from: this.props.user.uid,
+							to: chats[currentChat].uid,
+							message: msg,
+						})
+					}}/>
 				</Grid>
 			</Grid>
 		);
